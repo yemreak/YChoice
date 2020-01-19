796081b01c50476f2021bf9b654608ab65d0ddc7
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-01-19 13:35:13 +0300

    👨‍🔬 İlk cache yapısı denemesi

--- choice.py
+++ choice.py
@@ -1,3 +1,5 @@
+KEYS = {}
+
 
 def compare(x, *keys, equal=False) -> bool:
 
@@ -20,8 +22,38 @@ def compare(x, *keys, equal=False) -> bool:
     return (equal and keys[0] == keys[1]) or _compare()
 
 
+def registerCache(result, key1, key2):
+
+    calcCount = 0
+
+    def _exists(value):
+        return value in KEYS.values
+
+    def _calcValue(value):
+        nonlocal calcCount
+        calcCount += 1
+
+        if result:
+            value += (1 / 2) ** calcCount
+        else:
+            value -= (1 / 2) ** calcCount
+
+        return value
+
+    def _addIfEmpty(key, value):
+        if not _exists(value):
+            KEYS[key] = value
+        else:
+            value = _calcValue(value)
+            _addIfEmpty(key, value)
+
+    pass
+
+
 def isBetter(key1, key2, equal=False) -> bool:
-    return compare(0, key1, key2)
+    result = compare(0, key1, key2)
+    registerCache(result, key1, key2)
+    return result
 
 
 def isWorse(key1, key2, equal=False) -> bool:
