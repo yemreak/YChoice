616ef6bdd190ced3ed88d369253fbccfa1da3192
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-01-19 12:39:35 +0300

    ✨ Karşılaştırma metotları sadeleştirildi

--- choice.py
+++ choice.py
@@ -1,23 +1,28 @@
-def compare(x, *keys) -> bool:
-    msg = "Büyük olanı seçin\n"
-    msg += "Seçin - Sayı\n"
-    msg += "----------------\n"
-    for i, key in enumerate(keys):
-        msg += f"{i} - {key}\n"
 
-    while(True):
-        try:
-            y = int(input(msg))
-            break
-        except ValueError:
-            print("Hatalı seçim, tekrar deneyin")
+def compare(x, *keys, equal=False) -> bool:
 
-    return x == y
+    def _compare():
+        msg = "Büyük olanı seçin\n"
+        msg += "Seçin - Sayı\n"
+        msg += "----------------\n"
+        for i, key in enumerate(keys):
+            msg += f"{i} - {key}\n"
+
+        while(True):
+            try:
+                y = int(input(msg))
+                break
+            except ValueError:
+                print("Hatalı seçim, tekrar deneyin")
+
+        return x == y
+
+    return (equal and keys[0] == keys[1]) or _compare()
 
 
 def isBetter(key1, key2, equal=False) -> bool:
-    return (equal and key1 == key2) or compare(0, key1, key2)
+    return compare(0, key1, key2)
 
 
 def isWorse(key1, key2, equal=False) -> bool:
-    return (equal and key1 == key2) or compare(1, key1, key2)
+    return compare(1, key1, key2)
